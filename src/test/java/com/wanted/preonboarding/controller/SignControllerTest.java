1d9a0de43ec87d5b5f69a5925ab8782b753c9cfe
uknow-home <yoon6763@naver.com>
2023-08-16 11:36:10 +0900

    Sign 테스트 코드 추가

---
+++ src/test/java/com/wanted/preonboarding/controller/SignControllerTest.java
@@ -0,0 +1,61 @@
+package com.wanted.preonboarding.controller;
+
+import com.wanted.preonboarding.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SignControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void signUpSuccess() throws Exception {
+        // Given
+        String email = "testuser@example.com14";
+        String password = "password2";
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.post("/sign-api/sign-up")
+                        .param("email", email)
+                        .param("password", password))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - email 중복")
+    void signUpFailEmail() throws Exception {
+        // Given
+        String email = "testuser@example.com13";
+        String password = "password2";
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.post("/sign-api/sign-up")
+                        .param("email", email)
+                        .param("password", password))
+                .andExpect(status().isOk());
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.post("/sign-api/sign-up")
+                        .param("email", email)
+                        .param("password", password))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+    }
+}
